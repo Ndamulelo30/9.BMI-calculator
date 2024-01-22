4f9bbfc72ed715f4b61cd99bcc9f63f291b7e556
Sanele <Sanele@LAPTOP-FC04BNU2>
2024-01-22 08:15:54 +0200

    Add project files.

---
+++ 9.BMI calculator/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaration of variables
+        Dim height, weight, bmi As Double
+        weight = Val(TextBox1.Text)
+        height = Val(TextBox2.Text)
+
+        bmi = weight / height ^ 2  'BMI formula
+
+        TextBox3.Text = bmi
+
+        If bmi < 18 Then
+            TextBox4.Text = "You are underweight"
+        ElseIf 18 <= bmi And bmi < 30 Then
+            TextBox4.Text = "You are Normal"
+        Else
+            TextBox4.Text = "You are overweight"
+        End If
+    End Sub
+End Class
